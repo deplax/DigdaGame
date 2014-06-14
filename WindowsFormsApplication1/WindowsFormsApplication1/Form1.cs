44e238490b29cab66fac7de360b87b0519741a73
Jeong Won Cheol <sturdyegg@gmail.com>
2014-06-14 18:45:54 +0900

    Ah...

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,48 @@ namespace WindowsFormsApplication1
 {
         public partial class Form1 : Form
         {
+	
+
 	public Form1()
 	{
 	        InitializeComponent();
+	        timer1.Interval = 100;
+	        timer1.Enabled = true;
+	        
+	}
+
+	private void Form1_Load(object sender, EventArgs e)
+	{
+	}
+
+	private void Form1_Click(object sender, EventArgs e)
+	{
+	}
+
+	public void print()
+	{
+	        Bitmap screen = new Bitmap(700, 700);
+	        Graphics g = Graphics.FromImage(screen);
+
+	        Bitmap b1 = Properties.Resources.test01;
+	        g.DrawImage(b1, 0, 0);
+	        g.Dispose(); 
+	}
+
+	void update()
+	{
+
+	}
+
+	void draw()
+	{
+	        print();
+	}
+
+	private void timer1_Tick_1(object sender, EventArgs e)
+	{
+	        update();
+	        draw();
 	}
         }
 }
